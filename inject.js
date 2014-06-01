5fd8982e4a6f13a4e53bcaf0cc8c080e12e3db5b
Mihai Iachimovschi <mihai.iachimovschi@gmail.com>
2014-06-02 00:23:29 +0200

    Extending Array. Creating the merge left functionality.

--- inject.js
+++ inject.js
@@ -1,7 +1,74 @@
+/**
+ * Array extension.
+ *
+ * @returns {Array}
+ */
+Array.prototype.clone = function() {
+    return this.slice(0);
+};
+
 function AI() {
     this.alterHtml();
     this.game = new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
 }
+/**
+ * Helper function that will shift all tiles to left.
+ *  0 2 2 4  -> 2 2 4 0
+ *  2 0 2 8  -> 2 2 8 0
+ *  4 8 0 4  -> 4 8 4 0
+ *  0 0 2 0  -> 2 0 0 0
+ *
+ * @param {Array} input This is the input array with 16 elements.
+ * @returns {Array} The output array with 16 elements in it.
+ */
+AI.prototype.shiftLeft = function(input) {
+    for(var i = 0; i < 4; i++) {
+        var empty = -1;
+        for(var j = 0; j < 4; j++) {
+            var key = i*4 + j;
+            if(input[key] == 0) {
+                empty = j;
+                continue;
+            }
+
+            if(empty >= 0) {
+                var emptyKey = i*4 + empty;
+                input[emptyKey] = input[key];
+                input[key] = 0;
+                empty++;
+            }
+        }
+    }
+    return input;
+};
+
+/**
+ * Merges the tiles to the left. Before merging, shifts left them.
+ *
+ * @param {Array} input
+ * @returns {*[]} This is the result and the score gained after the move.
+ */
+AI.prototype.mergeLeft = function(input) {
+    var result = input.clone();
+    result = this.shiftLeft(result);
+    var score = 0
+    for(var i = 0; i < 4; i++) {
+        for(var j = 0; j < 4; j++) {
+            var key = i*4 + j;
+            if(j < 3 && result[key] == result[key+1]) {
+                result[key] = result[key] * 2;
+                score += result[key];
+                for(var k = j + 1; k < 3; k++) {
+                    var searchKey = i*4 + k;
+                    result[searchKey] = result[searchKey + 1];
+                    result[searchKey + 1] = 0;
+                }
+            }
+        }
+    }
+    return [result, score];
+};
+
 
 AI.prototype.alterHtml = function() {
     var controlDiv = document.createElement('div');
