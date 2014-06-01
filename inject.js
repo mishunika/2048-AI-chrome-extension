d7dee61e8fa116d7a0e45bd6279bbeefcf599581
Mihai Iachimovschi <mihai.iachimovschi@gmail.com>
2014-06-01 09:29:41 +0200

    Initial commit.

---
+++ inject.js
@@ -0,0 +1,26 @@
+function AI() {
+    this.alterHtml();
+    this.game = new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
+}
+
+AI.prototype.alterHtml = function() {
+    var controlDiv = document.createElement('div');
+    controlDiv.className = 'above-game';
+    controlDiv.innerHTML = '<p class="game-intro">Additional AI controls</p> <a class="restart-button">Start Solver</a>';
+    var gameContainer = document.getElementsByClassName('game-container')[0];
+    var container = gameContainer.parentNode;
+    container.insertBefore(controlDiv, gameContainer);
+};
+
+AI.prototype.tick = function() {
+    console.log(this);
+    console.log(self);
+    this.game.move(parseInt((Math.random()*10)%2))
+};
+
+function init() {
+    window.ai = new AI();
+    window.setInterval(function(){ai.tick();}, 1000);
+}
+
+window.setTimeout(init, 100);
